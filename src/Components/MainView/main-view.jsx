3b98e1f371466f5e15afbff30d5bfcb25148cb91
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-02-02 15:06:15 -0600

    Updated code

--- src/Components/MainView/main-view.jsx
+++ src/Components/MainView/main-view.jsx
@@ -1,51 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 
 export const MainView = () => {
-    const [movie, setmovies] = useState([
-        {
-            id: 1,
-            title: 'Dune: Part Two',
-            description: '2024 American epic science fiction film directed and co-produced by Denis Villeneuve, who co-wrote the screenplay with Jon Spaihts.',
-            imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Dune_Part_Two_poster.jpeg/220px-Dune_Part_Two_poster.jpeg",
-            director: 'Denis Villeneuve',
-            genre: 'scifi',
-        },
-        {
-            id: 2,
-            title: 'Atlas',
-            description: ' 2024 American science fiction action film starring Jennifer Lopez as a highly skilled counterterrorism analyst, who harbors a profound skepticism towards artificial intelligence, and who comes to realize that it may be her sole recourse following the failure of a mission aimed at apprehending a rogue robot.',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Atlas_2024_film_poster.png/220px-Atlas_2024_film_poster.png',
-            director: 'Brad Peyton',
-            genre: 'scifi',
-        },
-        {
-            id: 3,
-            title: 'Code 8: Part II',
-            description: ' 2024 Canadian superhero science fiction action film[1] directed by Jeff Chan, who co-wrote the screenplay with Chris Paré, Sherren Lee and Jesse LaVercombe.',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Code_eight_part_II_poster.jpg/220px-Code_eight_part_II_poster.jpg',
-            director: 'Brad Peyton',
-            genre: 'scifi',
-        },
-
-    ]);
-
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
+    useEffect(() => {
+        fetch("https://film-app-f9566a043197.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        imageURL: doc.imageURL,
+                        description: doc.desription,
+                        director: doc.director,
+                        genre: doc.genre,
+                    };
+                });
+
+                setMovies(moviesFromApi);
+            });
+    }, []);
     if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)}
+            />
         );
     }
 
-    if (movie.length === 0) {
+    if (movies.length === 0) {
         return <div>The list is empty!</div>;
     }
 
     return (
         <div>
-            {movie.map((movie) => (
+            {movies.map((movie) => (
                 <MovieCard
                     key={movie.id}
                     movie={movie}
--- src/Components/MovieCard/movie-card.jsx
+++ src/Components/MovieCard/movie-card.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -5,7 +8,22 @@ export const MovieCard = ({ movie, onMovieClick }) => {
                 onMovieClick(movie);
             }}
         >
+            <img
+                src={movie.imageUrl}
+                alt={movie.title}
+            />
             {movie.title}
         </div>
     );
+};
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        // id: PropTypes.string.isRequired,
+        // title: PropTypes.string.isRequired,
+        // imageUrl: PropTypes.string.isRequired,
+        // description: PropTypes.string.isRequired,
+        // director: PropTypes.string.isRequired,
+        // genre: PropTypes.string.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };

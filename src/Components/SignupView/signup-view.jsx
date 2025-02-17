bfec93770559311157c341bb0d6a057b1cd12ca2
Troy M. Oubre <Oubre1@att.net>
2025-02-17 19:51:31 +0000

    Added container

--- src/Components/SignupView/signup-view.jsx
+++ src/Components/SignupView/signup-view.jsx
@@ -1,52 +1,81 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const SignupView = () => {
     const [userId, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-    const [birthdate, setBirthday] = useState("");
+    const [birthDate, setBirthday] = useState("");
 
-    const handleSubmit = (event) => { };
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                UserId:
-                <input
-                    type="text"
-                    value={userId}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="3"
+    const handleSubmit = (event) => { 
+        event.preventDefault();
+        const data = {
+            userId: userId,
+            password: password,
+            email: email,
+            birthDate: birthdate
+          };
+
+          fetch("https://film-app-f9566a043197.herokuapp.com/users", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }).then((response) => {
+            if (response.ok) {
+              alert("Signup successful");
+              window.location.reload();
+            } else {
+              alert("Signup failed");
+            }
+          });
+        };
+
+        return (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="signUpFormuserId">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={userId}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  minLength="3"
                 />
-            </label>
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
+              </Form.Group>
+        
+              <Form.Group controlId="signUpFormPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
-            </label>
-            <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
+              </Form.Group>
+              <Form.Group controlId="signUpFormEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
-            </label>
-            <label>
-                Birthday:
-                <input
-                    type="date"
-                    value={birthdate}
-                    onChange={(e) => setBirthday(e.target.value)}
-                    required
+              </Form.Group>
+              <Form.Group controlId="signUpFormBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={birthDate}
+                  onChange={(e) => setBirthday(e.target.value)}
+                  required
                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    );
-};
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          );
+        };
